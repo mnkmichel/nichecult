6416bd3bfe17813392577422b57a9099e914f721
monika <mmm.monikamichel@gmx.de>
2026-05-13 00:45:56 +0200

    QR_Github testlauf

--- strato-api/config.php
+++ strato-api/config.php
@@ -20,6 +20,8 @@ return [
         'allow_origins' => [
             'http://localhost:3000',
             'http://127.0.0.1:3000',
+            'http://localhost:3002',
+            'http://127.0.0.1:3002',
             'http://nichecult.de',
             'http://www.nichecult.de',
             'https://nichecult.de',
